5b76d79d3b182fa5ba70e1cbb5434ba007f9aa99
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-01-02 12:35:48 -0600

    post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,26 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+      puts 'yo'
+    else
+      puts 'not saved'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:message)
+  end
 end
